f05e0ebde14f3c886d8730e2ffdb840a7460b273
邹军华 <zoujunhua@meizu.com>
2015-10-23 10:33:21 +0800

    Fixed the problem about "Unsigned short value out of range" when make framework
    You can copy the custom.jar to your device to solve the problem.

---
+++ custom_jar.sh
@@ -0,0 +1,16 @@
+
+jarBaseName=$1
+tempSmaliDir=$2
+
+dir1=android/accounts/
+dir2=android/print/
+dir3=android/printservice/
+dir4=android/speech/
+
+if [ "$jarBaseName" = "framework" ];then
+	echo ">>> in custom_jar $jarBaseName"
+        mv $tempSmaliDir/smali/$dir1 $tempSmaliDir/smali_classes2/$dir1
+        mv $tempSmaliDir/smali/$dir2 $tempSmaliDir/smali_classes2/$dir2
+        mv $tempSmaliDir/smali/$dir3 $tempSmaliDir/smali_classes2/$dir3
+        mv $tempSmaliDir/smali/$dir4 $tempSmaliDir/smali_classes2/$dir4
+fi
